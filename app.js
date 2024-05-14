96be57ca913a9eba7c896d7209f12bc29ba10a16
muskangaffar85 <157056618+muskangaffar85@users.noreply.github.com>
2024-05-15 03:16:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('Erro!please enter a valid password')
+
+alert('Welcome to JS Land,,,\n Happy Coding!')
+
+alert('Welcome to JS Land,,,')
+
+alert('Happy Coding!\n' + `☑` +' Prevent this page from creating additional dialogs');
+ 
+
+alert('Hello...i can run JS through my web browser console')
